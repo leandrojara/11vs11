e9d96861c7c35ec1afc48dcabdbc55fe2f1a05f9
leandrojara <leandro.souza.jara@hotmail.com>
2017-09-26 20:07:38 -0400

    MainActivity

--- App/app/src/main/java/br/com/a11vs11/app/activity/MainActivity.java
+++ App/app/src/main/java/br/com/a11vs11/app/activity/MainActivity.java
@@ -1,17 +1,32 @@
 package br.com.a11vs11.app.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import br.com.a11vs11.app.R;
 import br.com.a11vs11.app.db.DBManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnCbfvStore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         DBManager.updateDB(this);
+
+        btnCbfvStore = (Button) findViewById(R.id.btnCbfvStore);
+        btnCbfvStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://cbfvstore.com.br/"));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
